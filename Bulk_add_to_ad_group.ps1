662a40ac9af223346329e727ede86a495609f5c2
Tima Hancock <tima.hancock13@gmail.com>
2025-07-11 10:40:27 -0400

    Add files via upload
    Add Bulk_add_to_ad_group.ps1 file

---
+++ Bulk_add_to_ad_group.ps1
@@ -0,0 +1,7 @@
+﻿# Create variable $users and initialize with CSV of users
+     $users = Import-Csv "path\to\text.csv"
+
+# Loop through all users in CSV, -Whatif is to simulate, remove when you want to fully execute
+     foreach ($user in $users) {
+         Add-ADGroupMember -Identity $user.groupname -Members $user.username -Whatif
+     }
